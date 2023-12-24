4b95db9031a47dfbf996c0cc77f167aa7bfdf71f
Arvind <europa1613@users.noreply.github.com>
2023-12-23 18:48:15 -0600

    top level blocks

---
+++ _0_tf-basics/01-first-ec2/1_ec2-instance.tf
@@ -0,0 +1,25 @@
+# Terraform Settings Block
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      #version = "~> 3.21" # Optional but recommended in production
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  profile = "default" # AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials
+  region  = "us-east-1"
+}
+
+# Resource Block
+resource "aws_instance" "ec2demo" {
+  ami           = "ami-079db87dc4c10ac91" #"ami-0533f2ba8a1995cf9" # Amazon Linux in us-east-1, update as per your region
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ec2demo"
+  }
+}
