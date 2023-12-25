f21e8c11fcfea6b8f28fc5cf257d8acbd382225f
Arvind <europa1613@users.noreply.github.com>
2023-12-24 20:35:04 -0600

    meta-data correction + input vars + datasources + outputs initial

--- _0_tf-basics/04-inputs-datasources-outputs/app1-install.sh
+++ _0_tf-basics/04-inputs-datasources-outputs/app1-install.sh
@@ -7,7 +7,6 @@ sudo service httpd start
 sudo echo '<h1>Welcome to StackSimplify - APP-1</h1>' | sudo tee /var/www/html/index.html
 sudo mkdir /var/www/html/app1
 sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Welcome to Stack Simplify - APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
-# below is aws metadata of the instance - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instancedata-data-retrieval.html
-TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"`
-#sudo curl -H "X-aws-ec2-metadata-token: $TOKEN"  http://169.254.169.254/latest/meta-data/ -o /var/www/html/app1/metadata.html
-sudo curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
+sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
+
+
