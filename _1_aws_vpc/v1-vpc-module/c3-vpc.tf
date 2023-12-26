5584b881b5a24de81a39e1d28b95ef6fc128963a
Arvind <europa1613@users.noreply.github.com>
2023-12-25 21:38:33 -0600

    version upgrades for working vpc

--- _1_aws_vpc/v1-vpc-module/c3-vpc.tf
+++ _1_aws_vpc/v1-vpc-module/c3-vpc.tf
@@ -1,20 +1,19 @@
 # Create VPC Terraform Module
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.78.0"  
-  # version = "~> 2.78"
+  version = "5.4.0"
 
   # VPC Basic Details
-  name = "vpc-dev"
-  cidr = "10.0.0.0/16"   
-  azs                 = ["us-east-1a", "us-east-1b"]
-  private_subnets     = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets      = ["10.0.101.0/24", "10.0.102.0/24"]
+  name            = "vpc-dev"
+  cidr            = "10.0.0.0/16"
+  azs             = ["us-east-1a", "us-east-1b"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
 
   # Database Subnets
-  create_database_subnet_group = true
-  create_database_subnet_route_table= true
-  database_subnets    = ["10.0.151.0/24", "10.0.152.0/24"]
+  create_database_subnet_group       = true
+  create_database_subnet_route_table = true
+  database_subnets                   = ["10.0.151.0/24", "10.0.152.0/24"]
 
   #create_database_nat_gateway_route = true
   #create_database_internet_gateway_route = true
@@ -25,7 +24,7 @@ module "vpc" {
 
   # VPC DNS Parameters
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
 
   public_subnet_tags = {
     Type = "public-subnets"
@@ -40,8 +39,9 @@ module "vpc" {
   }
 
   tags = {
-    Owner = "kalyan"
+    Owner       = "Arvind Rukmaji"
     Environment = "dev"
+    Terraform   = "true"
   }
 
   vpc_tags = {
